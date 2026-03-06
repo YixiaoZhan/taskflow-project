a9cb9ba80c0ddaf99bd22ab859ec37d509853c8c
Yixiao <zhanyixiao06@gmail.com>
2026-03-06 10:42:45 +0100

    añadir comentarios a js para facilitar la compresión

--- FCT_2/app.js
+++ FCT_2/app.js
@@ -6,17 +6,20 @@ const buscador = document.getElementById("buscador");
 const creador = document.getElementById("creador");
 const tareas = document.getElementById("tareas");
 
+//borrar valores del formulario
 const borrarFormulario = () => {
     creador.value = "";
 }
 
+//funcion para crear tareas
 const crearTarea = (tareaActual, tareas) => {
     const div = document.createElement("div");
     div.classList.add("tarea");
 
     const p = document.createElement("p");
     p.textContent = tareaActual + "\u00A0";
-    
+
+    //crear boton para borrar
     const button = document.createElement("button");
     button.textContent = "Borrar";
     button.classList.add("borrar");
@@ -28,13 +31,14 @@ const crearTarea = (tareaActual, tareas) => {
     tareas.append(div);
 }
 
+//funcion para buscar tarea
 const buscarTarea = () => {
     const valorBuscador = buscador.value;
     console.log(valorBuscador);
     document.querySelectorAll(".tarea").forEach(div => {
     const p = div.querySelector("p");
     const tarea = p.textContent.toLowerCase();
-
+    //presentacion de la tarea
     if (tarea.includes(valorBuscador)) {
       div.style.display = "block";
     } else {
@@ -43,10 +47,12 @@ const buscarTarea = () => {
   })
 }
 
+//Crear tarea
 boton.addEventListener("click", (e) =>{
     e.preventDefault();
     tareaActual = creador.value.trim();
 
+    //comprobar que no sea repetido o vacio
     if (tareaActual === "") {
     borrarFormulario();
     return alert("La tarea no puede estar vacío");
@@ -60,6 +66,7 @@ boton.addEventListener("click", (e) =>{
     borrarFormulario();
 })
 
+//Buscador
 buscador.addEventListener("input", (buscarTarea));
 
 
